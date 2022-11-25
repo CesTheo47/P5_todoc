a7451bc18ca52dfb3ec6a55f7d9ecde8dda8b8d4
theophile cestaro <theophilecestaro96@gmail.com>
2022-11-25 18:18:12 +0100

    update DAO

--- app/src/main/java/com/cleanup/todoc/database/dao/ProjectDao.java
+++ app/src/main/java/com/cleanup/todoc/database/dao/ProjectDao.java
@@ -1,10 +1,16 @@
 package com.cleanup.todoc.database.dao;
 
 import androidx.room.Insert;
+import androidx.room.Query;
 
 import com.cleanup.todoc.model.Project;
 
+import java.util.List;
+
 public interface ProjectDao {
+    @Query("SELECT * FROM Project" )
+    List<Project> getProjects();
+
     @Insert
     long insertProject(Project project);
 
